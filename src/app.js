9ca5635a22428fa66550874fa65f5b15e094f414
Radu Zaharia <101859038+raduzaharia-medium@users.noreply.github.com>
2024-12-12 20:04:53 +0000

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import Route from "./components/route";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Route></Route>
+    </div>
+  );
+}
+
+export default App;
